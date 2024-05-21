1b38a831edf313f9ab4671ce0f876f3e80aa5557
Muneeb Nawaz <muneebnawaz2018@gmail.com>
2024-05-21 12:53:03 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [forecasts, setForecasts] = useState();
+
+  useEffect(() => {
+    populateWeatherData();
+  }, []);
+
+  const contents =
+    forecasts === undefined ? (
+      <p>
+        <em>
+          Loading... Please refresh once the ASP.NET backend has started. See{" "}
+          <a href="https://aka.ms/jspsintegrationreact">
+            https://aka.ms/jspsintegrationreact
+          </a>
+          for more details.
+        </em>
+      </p>
+    ) : (
+      <table className="table table-striped" aria-labelledby="tableLabel">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecasts.map((forecast) => (
+            <tr key={forecast.date}>
+              <td>{forecast.date}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id="tableLabel">Weather forecast</h1>
+      <p>This component demonstrates fetching data from the server.</p>
+      {contents}
     </div>
   );
+
+  async function populateWeatherData() {
+    const response = await fetch("https://localhost:44353/weatherforecast");
+    const data = await response.json();
+    setForecasts(data);
+  }
 }
 
 export default App;
